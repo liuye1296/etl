154b71cd5482bf22d70e554d4db6617e6bcc990e
OJH <775954561@qq.com>
2021-01-11 15:36:33 +0800

    ajax版本1

---
+++ ajax.js
@@ -0,0 +1,43 @@
+function ajax(options) {
+        //将默认参数写入
+        function extend(obj1,obj2) {
+            for (let key in obj2){
+                obj1[key] = obj2[key]
+            }
+        }
+        //默认参数
+        let defaults ={
+            async:true
+        };
+        //extend使用，写入参数
+        extend(defaults,options);
+
+        //原生ajax请求封装
+        var xhr = new XMLHttpRequest();
+        //get请求
+        if (defaults.type.toLowerCase() == "get"){
+            if(defaults.data){
+                defaults.url += '?' + defaults.data;
+            }
+            xhr.open(defaults.type,defaults.url,defaults.async);
+            xhr.send(null);
+        }else{
+            //post请求
+            xhr.open(defaults.type,defaults.url,defaults.async);
+            xhr.setRequestHeader('content-type',"application/x-www-form-urlencoded");
+            xhr.send(defaults.data);
+        }
+        //返回接受的数据
+        xhr.onreadystatechange=()=>{
+            if(xhr.readyState == 4){
+                if (xhr.status == 200){
+                    if (defaults.success){//如果有数据就返回
+                        defaults.success(xhr.responseText);
+                    }
+                }else{
+                    alert('错误代码：'+xhr.status);
+                }
+            }
+        }
+
+}
---
+++ init.js
@@ -0,0 +1,19 @@
+(function () {
+    let list = document.getElementById('list'); //整个list的页面
+    let pages = document.getElementById('pages'); //pages下面的总页数
+    let btn = document.getElementById('btn'); //升降序的按钮
+    let iPages = 1; //第一页的数据(页的数据)
+    let num = 10; //每一页为10条数据
+
+    //初始化 获取第一页的内容渲染到页面
+    function init() {
+        ajax2({
+            type:'get',
+            url:'api/010.getdata.php',
+            data:'page='+iPages+'&num='+num,
+            success:function (str) {
+                console.log(str)
+            }
+        })
+    }
+})
